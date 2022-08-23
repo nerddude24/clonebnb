f77bb7d9c09b9390ee10c675a4d60e28b84ab29e
HeroGamesDz <herogamespro@gmail.com>
2022-08-24 00:14:38 +0100

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>yo</h1>
+		</div>
+	);
 }
